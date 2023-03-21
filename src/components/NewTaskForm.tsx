ed261124d2db1850cfbdecea50e6f572637f7c92
Patryk Czekaj <patryk13290@gmail.com>
2023-03-21 14:25:24 +0100

    added priority with sorting from high to low

--- src/components/NewTaskForm.tsx
+++ src/components/NewTaskForm.tsx
@@ -1,7 +1,12 @@
 export default function NewTaskForm(props: any) {
     return (
         <form onSubmit={props.onSubmit}>
-            <input type="text" name="text" value={props.inputValue} onChange={props.onChange} />
+            <select name="priority" value={props.NewTaskData.priority} onChange={props.onChange}>
+                <option value={1}>High</option>
+                <option value={2}>Medium</option>
+                <option value={3}>Low</option>
+            </select>
+            <input type="text" name="text" value={props.NewTaskData.text} onChange={props.onChange} />
             <button type="submit">Add</button>
         </form>
     )
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -5,26 +5,40 @@ interface Task {
     id: number;
     text: string;
     done: boolean;
+    priority: number;
+}
+interface NewTask {
+    text: string;
+    priority: string;
 }
 
 export default function ToDoList() {
     const [tasks, setTasks] = useState<Task[]>([]);
-    const [inputValue, setInputValue] = useState<string>("");
+    const [inputValue, setInputValue] = useState<NewTask>({
+        text: "",
+        priority: "3"
+    });
+
+    const priorities = ["high", "medium", "low"]
 
     function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
-        setInputValue(event.target.value);
+        setInputValue((prev) => ({
+            ...prev,
+            [event.target.name]: event.target.value
+        }));
     }
 
     function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
-        if (!inputValue) return;
+        if (!inputValue.text) return;
         const newTask: Task = {
             id: Date.now(),
-            text: inputValue,
+            text: inputValue.text,
             done: false,
+            priority: parseInt(inputValue.priority),
         };
         setTasks([...tasks, newTask]);
-        setInputValue("");
+        setInputValue({ text: "", priority: "3" });
     }
 
     function toggleDone(taskId: number) {
@@ -39,24 +53,33 @@ export default function ToDoList() {
         });
         setTasks(updatedTodos);
     }
+    const sortedTasks = [...tasks].sort((a, b) => a.priority - b.priority)
+    let prevPriority = 0
     return (
         <div>
-            <NewTaskForm onSubmit={handleSubmit} inputValue={inputValue} onChange={handleInputChange} />
+            <NewTaskForm onSubmit={handleSubmit} NewTaskData={inputValue} onChange={handleInputChange} />
             <ul>
                 {/*lista tasków */}
                 {
-                    tasks.map((item) => (
-                        <li
-                            key={item.id}
-                            style={{
-                                textDecoration: item.done ? "line-through" : "",
-                                cursor: "pointer"
-                            }}
-                            onClick={() => toggleDone(item.id)}
-                        >
-                            {item.text}
-                        </li>
-                    ))
+                    sortedTasks.map((item) => {
+                        let renderPriority = prevPriority !== item.priority
+                        prevPriority = item.priority
+                        console.log(item.priority)
+                        return (
+                            <span key={item.id}>
+                                {renderPriority && <h2>{priorities[item.priority - 1]} priority</h2>}
+                                <li
+                                    style={{
+                                        textDecoration: item.done ? "line-through" : "",
+                                        cursor: "pointer"
+                                    }}
+                                    onClick={() => toggleDone(item.id)}
+                                >
+                                    {item.text}
+                                </li>
+                            </span>
+                        )
+                    })
                 }
             </ul>
         </div>
