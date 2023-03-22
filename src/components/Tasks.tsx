c3666875b05d935f57aba3220df348aed0146267
Patryk Czekaj <patryk13290@gmail.com>
2023-03-22 20:51:40 +0100

    added edit and remove buttons

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,5 +1,7 @@
 import { Task } from './ToDoList'
+import { useState } from 'react'
 export default function Tasks({ content, toggle }: any) {
+    const [showButtons, setShowButtons] = useState<boolean>(false)
     const priorities = ["high", "medium", "low"]
     let prevPriority = 0
     return (
@@ -7,7 +9,7 @@ export default function Tasks({ content, toggle }: any) {
             let renderPriority = prevPriority !== item.priority
             prevPriority = item.priority
             return (
-                <span key={item.id}>
+                <span key={item.id} >
                     {renderPriority && <h2>{priorities[item.priority - 1]} priority</h2>}
                     <li
                         className='task-list-item'
@@ -15,9 +17,20 @@ export default function Tasks({ content, toggle }: any) {
                             textDecoration: item.done ? "line-through" : "",
                             cursor: "pointer"
                         }}
-                        onClick={() => toggle(item.id)}
+
+                        onMouseOver={() => setShowButtons(true)}
+                        onMouseLeave={() => setShowButtons(false)}
                     >
-                        {item.text}
+                        <span
+                            onClick={() => toggle(item.id)}
+                        >
+                            {item.text}
+                        </span>
+                        {showButtons &&
+                            <span className='task-buttons'>
+                                <button className='edit-task-button'>edit</button>
+                                <button className='remove-task-button'>x</button>
+                            </span>}
                     </li>
                 </span>
             )
